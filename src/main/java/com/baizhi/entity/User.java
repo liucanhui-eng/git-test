c73e449a61ee06610800c17630e02b2eac2ddf0c
test <1251303815@qq.com>
2018-06-10 09:15:32 +0800

    为user类添加Dao接口类

--- src/main/java/com/baizhi/entity/User.java
+++ src/main/java/com/baizhi/entity/User.java
@@ -8,8 +8,7 @@ public class User {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String name) { this.name = name;
     }
 
     public String getPassword() {
