c2d06c96e7ab63a284142e005dc3412461179edc
test <1251303815@qq.com>
2018-06-10 09:23:28 +0800

    为UserDao接口类添加查询所有方法

--- src/main/java/com/baizhi/dao/UserDao.java
+++ src/main/java/com/baizhi/dao/UserDao.java
@@ -2,6 +2,8 @@ package com.baizhi.dao;
 
 import com.baizhi.entity.User;
 
+import java.util.List;
+
 public interface UserDao {
     void insert();
 
@@ -9,4 +11,7 @@ public interface UserDao {
     void update(User user);
     //
     void delete(String id);
+    //
+    List<User> queryAll();
+
 }
